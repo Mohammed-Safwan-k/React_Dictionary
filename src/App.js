87b4f2b21444749f304e8e427729c9d1ef5b0355
Mohammed Safwan <safwanpklr@gmail.com>
2023-01-17 22:03:36 +0530

    created header search and language select

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import { Container } from '@material-ui/core';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
+  const [word, setWord] = useState("")
+  const [meanings, setMeanings] = useState([])
+  const [category, setCategory] = useState("en")
 
   const dictionaryApi = async () => {
     try {
+      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`)
 
+      setMeanings(data.data)
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi()
+  }, [word, category])
 
   return (
-    <div className="App">
-
+    <div className="App" style={{ height: "100vh", backgroundColor: "#342828", color: "white" }}>
+      <Container maxWidth="md" style={{ display: 'flex', flexDirection: 'column', height: "100vh" }}>
+        <Header category={category} setCategory={setCategory} word={word} setWord={setWord} />
+      </Container>
     </div>
   );
 }
