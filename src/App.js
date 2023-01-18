66105485d7c23c1a8aa34f1d7a22d8bbeef9b394
Mohammed Safwan <safwanpklr@gmail.com>
2023-01-18 21:56:34 +0530

    added definition section and meanings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container } from '@material-ui/core';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
+import Definitions from './components/Definitions/Definitions';
 import Header from './components/Header/Header';
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
     }
   };
 
-  console.log(meanings);
+  // console.log(meanings);
 
   useEffect(() => {
     dictionaryApi()
@@ -29,6 +30,7 @@ function App() {
     <div className="App" style={{ height: "100vh", backgroundColor: "#342828", color: "white" }}>
       <Container maxWidth="md" style={{ display: 'flex', flexDirection: 'column', height: "100vh" }}>
         <Header category={category} setCategory={setCategory} word={word} setWord={setWord} />
+        {meanings && (<Definitions word={word} meanings={meanings} category={category} />)}
       </Container>
     </div>
   );
