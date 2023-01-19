1d2c9a96bb1a8a52d8fe57149a7c422e1f7e43c3
Mohammed Safwan <safwanpklr@gmail.com>
2023-01-19 13:50:27 +0530

    project Completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,66 @@
-import { createTheme, MenuItem, TextField, ThemeProvider } from '@material-ui/core';
-import React from 'react';
-import './Header.css';
-import categories from '../../data/category';
+import { createTheme, TextField, ThemeProvider } from "@material-ui/core";
+import React from "react";
+import "./Header.css";
+import MenuItem from "@material-ui/core/MenuItem";
+import countries from "../../data/category";
+import { debounce } from "lodash";
 
-const Header = ({ setCategory, category, word, setWord }) => {
+const Header = ({
+  category,
+  setCategory,
+  setWord,
+  word,
+  setMeanings,
+  LightTheme,
+}) => {
   const darkTheme = createTheme({
     palette: {
       primary: {
-        main: '#fff'
+        main: LightTheme ? "#000" : "#fff",
       },
-      type: "dark"
+      type: LightTheme ? "light" : "dark",
     },
   });
 
-const handleChange = (language) => {
-  setCategory(language);
-  setWord("")
-}
+  const handleChange = (e) => {
+    setCategory(e.target.value);
+    setWord("");
+    setMeanings([]);
+  };
+
+    const handleText = debounce((text) => {
+    setWord(text);
+  }, 500);
 
   return (
-    <div className='header'>
-      <span className='title'>{word ? word : "Word Hunt"}</span>
-      <div className='inputs'>
+    <div className="header">
+      <span className="title">{word ? word : "Word Library"}</span>
+      <div className="inputs">
         <ThemeProvider theme={darkTheme}>
-          <TextField className='search' label="Search a word" value={word} onChange={(e) => setWord(e.target.value)} />
-          <TextField className='select' select label="Language" value={category} onChange={(e) => handleChange(e.target.value)}>
-            {categories.map((option) => (
+          <TextField
+            className="search"
+            id="filled-basic"
+            // value={word}
+            label="Search a Word"
+            onChange={(e) => handleText(e.target.value)}
+          />
+          <TextField
+            select
+            label="Language"
+            value={category}
+            onChange={(e) => handleChange(e)}
+            className="select"
+          >
+            {countries.map((option) => (
               <MenuItem key={option.label} value={option.label}>
                 {option.value}
               </MenuItem>
             ))}
-
           </TextField>
         </ThemeProvider>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
